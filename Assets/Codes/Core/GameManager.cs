469353d29b2674a59f748e8a4addf7035c790b22
WhiteFoxZ <fmjj008@gmail.com>
2026-03-07 13:26:52 +0900

    구글 스프레트 시트 데이터 가져오기 완료

--- Assets/Codes/Core/GameManager.cs
+++ Assets/Codes/Core/GameManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 /// <summary>
@@ -46,6 +48,61 @@ public class GameManager : MonoBehaviour
     public GameObject enemyCleaner; //적 정리기
 
 
+    [Header("게임 Item Data")]
+    public ItemData[] itemDatas; //아이템 데이터 참조 (추후 아이템 시스템 구현 시 사용)
+
+    const string URL = "https://docs.google.com/spreadsheets/d/1xHjfvfPxcGE9-rDfiwzXv-iw9ZQTfBDDMpSJ1rGrRQY/export?format=tsv&range=A2:H";
+
+    IEnumerator DownloadItemData()
+    {
+        //구글 스프레드시트에서 아이템 데이터 다운로드 (테스트용)
+        using (UnityWebRequest www = UnityWebRequest.Get(URL))
+        {
+            yield return www.SendWebRequest();
+
+            if (string.IsNullOrEmpty(www.error))
+            {
+                // Debug.Log("아이템 데이터 다운로드 성공!");
+                //다운로드한 데이터를 파싱하여 itemDatas 배열에 저장하는 로직 추가 필요
+                string data = www.downloadHandler.text;
+                // Debug.Log("다운로드한 데이터: " + data);
+                SetItemSO(data); //데이터 파싱 및 아이템 데이터 설정
+
+            }
+            else
+            {
+                Debug.LogError("아이템 데이터 다운로드 실패: " + www.error);
+            }
+        }
+    }
+
+    void SetItemSO(string tsv)
+    {
+        //item type	item id	이름(name)	item Desc	공격력(base Damage)	Base Count	LevelUp Damage	LevelUp Counts,관통력
+
+        string[] row = tsv.Split('\n');
+        int rowSize = row.Length;
+        int columnSize = row[0].Split('\t').Length;
+
+        for (int i = 0; i < rowSize; i++)
+        {
+            string[] column = row[i].Split('\t');
+
+            for (int j = 0; j < columnSize; j++)
+            {
+                //print(column[j]);
+
+                column[j] = column[j].Trim(); //공백 제거
+
+
+
+            }
+        }
+    }
+
+
+
+
     /// <summary>
     /// 시작 시 호출 - 싱글톤 설정 및 초기화
     /// </summary>
@@ -64,6 +121,8 @@ public class GameManager : MonoBehaviour
         else Destroy(gameObject);
 
 
+
+
     }
 
     /// <summary>
@@ -73,6 +132,17 @@ public class GameManager : MonoBehaviour
     {
         // Lobby에서 선택한 스테이지로 게임 시작
         GameStart(0);
+
+
+        //itemDatas 에 정보를 로그로 출력 (테스트용)
+        // foreach (var item in itemDatas)
+        // {
+        //     Debug.Log($"아이템: {item.itemName}, 유형: {item.itemType}, 데미지: {item.baseDamage}");
+        // }
+
+        StartCoroutine(DownloadItemData()); //아이템 데이터 다운로드 (테스트용)
+
+
     }
 
     /// <summary>
