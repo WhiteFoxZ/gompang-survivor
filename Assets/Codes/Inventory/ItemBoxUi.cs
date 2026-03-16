83bb1877ac2563106a643f9ec3a47fea011fd349
WhiteFoxZ <fmjj008@gmail.com>
2026-03-16 23:20:52 +0900

    111

--- Assets/Codes/Inventory/ItemBoxUi.cs
+++ Assets/Codes/Inventory/ItemBoxUi.cs
@@ -1,13 +1,33 @@
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemBoxUi : MonoBehaviour
 {
 
     public ShopItem _shopItem;
 
+    private Text[] buttonText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // 자식중에 text 컴포넌트중 2번째 text 에 _shopItem에 itemCnt을 보여준다
+        buttonText = GetComponentsInChildren<Text>();
+
+
+        this.Log($"ssss: {buttonText[1]}");
+
+
+        buttonText[1].text = _shopItem.price.ToString();
+
+        if (_shopItem.payType == PayType.PAY)
+        {
+            buttonText[0].text = _shopItem.itemCnt.ToString();
+
+        }
+
+
 
     }
 
