ba98d84ca193936ab40cfafa1669079f83d64617
WhiteFoxZ <fmjj008@gmail.com>
2026-04-16 22:16:35 +0900

    디버깅로그 에디터에서만..

--- Assets/Codes/Utils/Debug2.cs
+++ Assets/Codes/Utils/Debug2.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using System.Runtime.CompilerServices;
+using System.Diagnostics; // Conditional 사용을 위해 필요
 
-public static class Debug2
+public static class LogExtensions
 {
+    // UNITY_EDITOR 심볼이 있을 때만 이 함수가 실행됩니다. 
+    // 즉, 안드로이드 빌드 시에는 이 함수를 호출하는 곳 자체가 사라집니다.
+    [Conditional("UNITY_EDITOR")]
     [HideInCallstack]
     public static void Log(this object obj, string message, [CallerMemberName] string methodName = "")
     {
         // [클래스명 : 메서드명] 메시지 형태의 포맷
-        Debug.Log($"<color=#00FF00>[{obj.GetType().Name} : {methodName}]</color> {message}");
+        UnityEngine.Debug.Log($"<color=#00FF00>[{obj.GetType().Name} : {methodName}]</color> {message}");
     }
 }
